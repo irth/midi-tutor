971359aa46bbdda3c2cc95bc575e42cec6d617ea
Wojciech Kwolek <wojciech@kwolek.xyz>
2020-10-30 09:43:13 +0100

    remove old commented code

--- src/Music.jsx
+++ src/Music.jsx
@@ -16,24 +16,6 @@ export default function Music({
     if (target.current == null) return;
 
     target.current.innerHTML = "";
-    // const renderer = new VF.Renderer(target.current, VF.Renderer.Backends.SVG);
-    // renderer.resize(500, 500); // TODO: set this in a more responsive way
-    // const context = renderer.getContext();
-    // context.clear();
-    // const stave = new VF.Stave(10, 40, 400);
-    // stave.addClef("treble").addTimeSignature("4/4");
-
-    // if (notes != null && notes.length > 0) {
-    //   console.log(notes, 3);
-    //   var voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
-    //   voice.addTickables(notes);
-    //   var formatter = new VF.Formatter()
-    //     .joinVoices([voice])
-    //     .format([voice], 400);
-    // }
-
-    // stave.setContext(context).draw();
-    // voice.draw(context, stave);
     const vf = new Vex.Flow.Factory({
       renderer: { elementId: target.current, width: width, height: height },
     });
